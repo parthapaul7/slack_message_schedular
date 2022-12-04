3ff0869544a610488efcfb9766f7768fb9ec7e31
Partha <pparthapaul11@gmail.com>
2022-12-04 16:27:44 +0530

    docker changes

--- listeners/test.js
+++ listeners/test.js
@@ -1,37 +1,21 @@
-const { WebClient ,LogLevel} = require("@slack/web-api");
-
-// An access token (from your Slack app or custom integration - xoxp, xoxb)
-const userToken = process.env.USER_TOKEN;
-const botToken = process.env.BOT_TOKEN;
-
+const { WebClient, LogLevel } = require('@slack/web-api')
+    // An access token (from your Slack app or custom integration - xoxp, xoxb)
+const userToken = process.env.USER_TOKEN
+const botToken = process.env.BOT_TOKEN
 const client = new WebClient(userToken, {
-  logLevel: LogLevel.DEBUG,
-});
-
-// testing bull 
-
-const Queue  = require("bull");
-const sendMsg = new Queue("sendMsg",{
-  redis:{
-    host:"",
-    port:"",
-    password:"",
-  }
-});
-
-sendMsg.add("sendIt",{name: "test"}).then((result) => {
-  console.log("the result is", result);
-}).catch((error) => {
-  console.error(error);
-});
-
-sendMsg.process((job) => {
-  return console.log("the job is", job.data);
-});
-
-
-
-exports.test = async (app) => {
-  console.log("test is running");
-   
-};
+        logLevel: LogLevel.DEBUG,
+    })
+    // testing bull
+const { Queue } = require('bullmq')
+const { Worker } = require('bullmq')
+const myQueue = new Queue('my-queue')
+const worker = new Worker('my-queue', async(job) => {
+    console.log(job.data)
+})
+exports.test = async(app) => {
+    console.log('test is running')
+    myQueue.add('test', { foo: 'bar' }, { delay: 1000 })
+    await worker.on('completed', (job) => {
+        console.log('job completed')
+    })
+}
